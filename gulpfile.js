f8d4623b60b496611a9b4dbae69ac280c9ea6ebe
Martin Saulis <martin@saulis.com>
2014-02-05 00:52:14 +0200

    initial, gulp tests

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var server = require('tiny-lr')();
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var uglify = require('gulp-uglify');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var styl = require('gulp-styl');
+var livereload = require('gulp-livereload');
+var Q = require('q');
+
+gulp.task('scripts', ['listen'], function() {
+  gulp.src(['src/coffee/**/*.coffee'])
+    .pipe(coffee())
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('build/js'))
+    .pipe(livereload(server))
+});
+
+gulp.task('styles', ['listen'], function() {
+  gulp.src(['src/css/**/*.css'])
+    .pipe(styl({compress : true}))
+    .pipe(gulp.dest('build/css'))
+    .pipe(livereload(server))
+});
+
+gulp.task('listen', function(next) {
+  server.listen(35729, function(err) {
+    if(err) return console.log(err);
+    next();
+  });
+});
+
+gulp.task('default', ['listen', 'scripts', 'styles'], function() {
+  gulp.watch('src/**', ['scripts']);
+  gulp.watch('css/**', ['styles']);
+});
