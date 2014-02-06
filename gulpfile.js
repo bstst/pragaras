a69713b8a58bcdec5eefcff00ad05e31b8d55d0b
Martin Saulis <martin@saulis.com>
2014-02-06 23:19:50 +0200

    more build improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,47 @@
 var server = require('tiny-lr')();
 var gulp = require('gulp');
+var path = require('path');
 var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 var styl = require('gulp-styl');
 var livereload = require('gulp-livereload');
+var less = require('gulp-less');
 var Q = require('q');
 
-gulp.task('scripts', ['listen'], function() {
-  gulp.src(['src/coffee/**/*.coffee'])
+gulp.task('coffee', ['listen'], function(){
+  gulp.src([
+    'src/coffee/**/*Util*.coffee',
+    'src/coffee/**/*Model*.coffee',
+    'src/coffee/**/*Collection*.coffee',
+    'src/coffee/**/*View*.coffee',
+    'src/coffee/**/**.coffee'
+  ])
     .pipe(coffee({bare: true}).on('error', function(err){ console.log(err); }))
     .pipe(uglify())
-    .pipe(concat('all.min.js'))
+    .pipe(concat('all.js'))
     .pipe(gulp.dest('build/js'))
     .pipe(livereload(server))
 });
 
-gulp.task('styles', ['listen'], function() {
-  gulp.src(['src/css/**/*.css'])
+gulp.task('less', function (){
+  gulp.src('src/less/**/*.less')
+    .pipe(less({paths: [path.join(__dirname, 'less', 'includes')]}))
     .pipe(styl({compress : true}))
+    .pipe(concat('all.css'))
     .pipe(gulp.dest('build/css'))
     .pipe(livereload(server))
 });
 
-gulp.task('listen', function(next) {
-  server.listen(35729, function(err) {
+gulp.task('listen', function(next){
+  server.listen(35729, function(err){
     if(err) return console.log(err);
     next();
   });
 });
 
-gulp.task('default', ['listen', 'scripts', 'styles'], function() {
-  gulp.watch('src/**', ['scripts']);
-  gulp.watch('css/**', ['styles']);
+gulp.task('default', ['listen', 'coffee', 'less'], function(){
+  gulp.watch('src/coffee/**', ['coffee']);
+  gulp.watch('src/less/**', ['less']);
 });
