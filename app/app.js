827ef9eeb37103c9d39d2e932cf0cdad78739ad3
Martin Saulis <martin@saulis.com>
2014-02-08 22:47:24 +0200

    unused package

--- app/app.js
+++ app/app.js
@@ -9,7 +9,6 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-var engines = require('consolidate');
 
 var app = express();
 
